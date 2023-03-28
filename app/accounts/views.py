e3e0ea484d191278a50651dc5097d2bc27e4c52c
Alibek Beket <alibek1beket@gmail.com>
2023-03-28 11:26:10 +0600

    добавил в accounts/views функции для регистрации, входа, выхода из аккаунта и просмотра всех постов аккаунта к которому мы зашли

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, CreateView, ListView
+
+from accounts.forms import LoginForm, CustomUserCreationForm
+
+from instagram.models import Posts
+
 
 # Create your views here.
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context=context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('projects_list')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('projects_list')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+
+class AccountView(ListView):
+    template_name = 'user_detail.html'
+    model = Posts
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['user'] = get_object_or_404(get_user_model(), pk=self.kwargs['pk'])
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=get_object_or_404(get_user_model(), pk=self.kwargs['pk']))
+        return queryset
--- app/app/urls.py
+++ app/app/urls.py
@@ -19,4 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),]
+    path('admin/', admin.site.urls),
+    path('auth/', include('accounts.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
